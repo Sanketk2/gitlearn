11d03274b0a00f6f0a337ced7bb09755d92d6fdf
sanket <sanketdoshik2@gmail.com>
2015-05-09 21:01:27 +0530

    initial commit

---
+++ anothergit/anothergit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  anothergit
+//
+//  Created by Sanket Doshi on 09/05/15.
+//  Copyright (c) 2015 Sanket Doshi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
